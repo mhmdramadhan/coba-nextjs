1c3f8eb76d9f3b5c4a63389d24ccd91f6d291db5
mhmdramadhan <mr.mhmd.ramadhan@gmail.com>
2023-08-16 05:55:05 +0700

    ADD : Limiter for request api

---
+++ coba-next3_using-api/src/app/api/hello/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import { limiter } from "../config/limiter";
+
+export async function GET(request: Request) {
+
+    const origin = request.headers.get('origin')
+
+    const remaining = await limiter.removeTokens(1)
+    console.log('====================================');
+    console.log('remaining : ', remaining);
+    console.log('====================================');
+
+    if (remaining < 0) {
+        return new NextResponse(null, {
+            status: 429,
+            statusText: "Too Many Request",
+            headers: {
+                'Access-Control-Allow-Origin': origin || '*',
+                'Content-Type': 'text/plain'
+            }
+        })
+    }
+
+    return NextResponse.json({ "message": "Success" })
+}
--- coba-next3_using-api/src/middleware.ts
+++ coba-next3_using-api/src/middleware.ts
@@ -1,6 +1,12 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request: Request) {
+
+    const regex = new RegExp('/api/*')
+    if(regex.test(request.url)){
+
+    }
+
     console.log('====================================');
     console.log('Middleware here');
     console.log(request.method);
@@ -9,4 +15,8 @@ export function middleware(request: Request) {
     console.log(origin);
     console.log('====================================');
     return NextResponse.next()
+}
+
+export const config = {
+    matcher: '/api/:path*'
 }
