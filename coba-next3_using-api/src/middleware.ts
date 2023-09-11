6763ae1d77591facd7a5440527dd0181ba508767
mhmdramadhan <mr.mhmd.ramadhan@gmail.com>
2023-09-12 06:01:03 +0700

    push

--- coba-next3_using-api/src/middleware.ts
+++ coba-next3_using-api/src/middleware.ts
@@ -1,18 +1,29 @@
 import { NextResponse } from "next/server";
 
+import allowedOrigins = process.env.NODE_ENV === "production"
+    ? ['https://www.yoursite.com', 'https://yoursite.com']
+    : ['http://localhost:3000', 'https://www.google.com'];
+
 export function middleware(request: Request) {
 
-    const regex = new RegExp('/api/*')
-    if(regex.test(request.url)){
+    const origin = request.headers.get('origin')
+    console.log(origin);
 
+    if (origin && !allowedOrigins.includes(origin)) {
+        return new NextResponse(null, {
+            status: 400,
+            statusText: "Bad Request",
+            headers: {
+                'Content-Type': 'text/plain'
+            }
+        })
     }
 
     console.log('====================================');
     console.log('Middleware here');
     console.log(request.method);
     console.log(request.url);
-    const origin = request.headers.get('origin')
-    console.log(origin);
+
     console.log('====================================');
     return NextResponse.next()
 }
